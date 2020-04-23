8fc14ec578e2337c1e1d3357b4e900eea316e4ef
hashirshoaeb <hashirshoaeb@gmail.com>
2020-04-24 00:59:08 +0500

    react starter

---
+++ dashboard/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hashir</p>
+    </div>
+  );
+}
+
+export default Login;
---
+++ dashboard/src/components/Login.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="container my-5">
+      <h1 className="display-4 text-center my-5"> Login</h1>
+      <div className="d-flex justify-content-center">
+        <div className="col-md-6">
+          <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+            <div className="card-body">
+              <form>
+                <div className="form-group">
+                  <label>Email:</label>
+                  <input
+                    type="email"
+                    className="form-control "
+                    placeholder="name@example.com"
+                  ></input>
+                </div>
+                <div className="form-group">
+                  <label>Password:</label>
+                  <input
+                    type="password"
+                    className="form-control "
+                    placeholder="Password"
+                  ></input>
+                </div>
+                <br></br>
+                <button className="btn btn-dark btn-lg btn-block">
+                  Sign up
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Login;
