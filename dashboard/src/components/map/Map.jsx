6333ffbcb67cc9c1c91dea4c3bdb98ebcf742bf0
hashirshoaeb <hashirshoaeb@gmail.com>
2020-05-31 18:53:08 +0500

    markers added

--- dashboard/src/components/map/Map.jsx
+++ dashboard/src/components/map/Map.jsx
@@ -2,6 +2,20 @@ import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
 import React, { Component } from "react";
 import { style, mapStyle } from "./style";
 
+const locations = [
+  { lat: 31.522355, lng: 74.436162 },
+  { lat: 31.522335, lng: 74.43574 },
+  { lat: 31.522315, lng: 74.436162 },
+  { lat: 31.522345, lng: 74.43574 },
+  { lat: 31.522355, lng: 74.436172 },
+  { lat: 31.522335, lng: 74.43594 },
+  { lat: 31.524029, lng: 74.437116 },
+  { lat: 31.524095, lng: 74.437374 },
+  { lat: 31.527824, lng: 74.434982 },
+  { lat: 31.527796, lng: 74.42804 },
+  { lat: 31.521017, lng: 74.434363 },
+];
+
 function _mapLoaded(mapProps, map) {
   map.setOptions({
     styles: mapStyle,
@@ -20,7 +34,11 @@ const MapContainer = (props) => {
       }}
       zoom={10}
       onReady={(mapProps, map) => _mapLoaded(mapProps, map)}
-    ></Map>
+    >
+      {locations.map((value, index) => {
+        return <Marker position={value}></Marker>;
+      })}
+    </Map>
   );
 };
 
