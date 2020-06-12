62e7646a8f05dba34e8e59c19b56fa8bdc6d8aa4
hashirshoaeb <hashirshoaeb@gmail.com>
2020-06-12 20:44:31 +0500

    cors enabled

--- server/app.js
+++ server/app.js
@@ -2,10 +2,11 @@ const patientRouter = require(`${__dirname}/routes/patientRoutes`);
 const hospitalRouter = require(`${__dirname}/routes/hospitalRoutes`);
 const express = require("express");
 const morgan = require("morgan");
-// const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/patients", patientRouter);
