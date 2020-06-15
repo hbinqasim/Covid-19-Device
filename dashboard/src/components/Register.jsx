733a4cc230f4decad41567992637992e85d98dad
hashirshoaeb <hashirshoaeb@gmail.com>
2020-06-15 16:36:59 +0500

    api correction

--- dashboard/src/components/Register.jsx
+++ dashboard/src/components/Register.jsx
@@ -28,7 +28,7 @@ const Register = () => {
   const onClick = (ev) => {
     ev.preventDefault();
     console.log({ name, cnic, age, address, phone, password });
-    Axios.post(`${process.env.REACT_APP_SRV_URL}/hospitals`, {
+    Axios.post(`${process.env.REACT_APP_SRV_URL}/patients`, {
       name: name,
       CNIC: cnic,
       address: address,
